8748f3eec6cb1e2d0c4deda351aa95f68b47118b
Atharv625 <patilatharv625@gmail.com>
2025-04-01 22:02:00 +0530

    added Day29.py

---
+++ Day29.py
@@ -0,0 +1,16 @@
+# its about docstring
+def square(n):
+    """
+    This function returns the square of a number.
+
+    Parameters:
+    n (int): The number to be squared.
+
+    Returns:
+    int: The square of the input number.
+    """
+    return n * n
+print(square(5))  # Output: 25
+print(square.__doc__)  # Output: The docstring of the 
+# PEP 8
+# PEP 8 is the style guide for Python code. It provides conventions for writing clean and readable code.'
