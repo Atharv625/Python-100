3ee80942f3cf78558d0ff01ffafd72ba47a00b5f
Atharv625 <patilatharv625@gmail.com>
2025-04-02 11:25:04 +0530

    Added Day31 and Day32

---
+++ Day31.py
@@ -0,0 +1,5 @@
+# its about set
+s={1,2,5,6,3,2,4,8,9,7}
+print(s)
+a={1,2,3,4,5}
+print(a)
---
+++ Day32.py
@@ -0,0 +1,11 @@
+s={2,5,6,3,9,8,7,4,1}
+s2={1,2,3,4,5}
+print(s.union(s2))  # Output: {1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(s.intersection(s2))  # Output: {1, 2, 3, 4, 5}
+print(s.difference(s2))  # Output: {6, 7, 8, 9}
+print(s.issubset(s2))  # Output: False
+print(s.issuperset(s2))  # Output: True
+print(s.symmetric_difference(s2))
+s.update(s2)
+print(s)  
+
