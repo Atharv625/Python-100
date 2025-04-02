e34ab02ede5d2ca28682dc5c9d644884b98a258a
Atharv625 <patilatharv625@gmail.com>
2025-04-02 11:50:50 +0530

    Modified  Day32

--- Day32.py
+++ Day32.py
@@ -6,6 +6,11 @@ print(s.difference(s2))  # Output: {6, 7, 8, 9}
 print(s.issubset(s2))  # Output: False
 print(s.issuperset(s2))  # Output: True
 print(s.symmetric_difference(s2))
-s.update(s2)
-print(s)  
+# s.update(s2)
+# print(s)  
+print(s2.issubset(s))  # Output: True
+print(s.isdisjoint(s2))  # Output: False
+print(s.discard(s2))  # Output: {1, 2, 3, 4, 5}
+del s2
+print(s2)  # Output: set()
 
